73081e78b5d87f2154fa8a2df246d5109675ba75
Ben Brubaker <heavensords1@gmail.com>
2017-06-16 22:38:26 -0400

    Basic site

--- src/app.js
+++ src/app.js
@@ -1,31 +1,5 @@
 (function () {
     var app = angular.module('civichack17App', ['DataManager', 'ui.router', 'chart.js', 'rzModule', 'angular-centered']);
-
-
-    // app.run(function ($state, $rootScope) {
-    //     $rootScope.$on('$stateChangeError', function (evt, toState, toParams, fromState, fromParams, error) {
-    //         if (angular.isObject(error) && angular.isString(error.code)) {
-    //             switch (error.code) {
-    //                 case 'NOT_AUTH':
-    //                     // go to the login page
-    //                     $state.go('home');
-    //                     break;
-    //                 case 'ALREADY_AUTH':
-    //                     //go to the dash board
-    //                     $state.go('user.history');
-    //                     break;
-    //                 default:
-    //                     // set the error object on the error state and go there
-    //                     $state.get('error').error = error;
-    //                     $state.go('error');
-    //             }
-    //         }
-    //         else {
-    //             // unexpected error
-    //             $state.go('home');
-    //         }
-    //     });
-    // });
     app.config(function ($stateProvider, $urlRouterProvider) {//, $locationProvider) {
         // $locationProvider.html5Mode(true);
 
